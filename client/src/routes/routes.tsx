e9267b06381a84f140853ddbe81ca0a33ed01a83
Phạm Huỳnh Đăng Khoa <khoapham1405@gmail.com>
2024-12-18 08:44:20 +0700

    fix route

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,10 +1,7 @@
 import LoadingScreen from '@/components/common/LoadingScreen'
 import DefaultLayout from '@/components/layout/DefaultLayout'
-<<<<<<< Updated upstream
 import { ComponentType, lazy, ReactNode, Suspense } from 'react'
-import { useRoutes } from 'react-router-dom'
-=======
->>>>>>> Stashed changes
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const Loadable = <P extends object>(Component: ComponentType<P>): React.FC<P> => {
   return (props: P): ReactNode => (
@@ -16,22 +13,6 @@ const Loadable = <P extends object>(Component: ComponentType<P>): React.FC<P> =>
 
 const Home = Loadable(lazy(() => import('@/pages/Home')))
 
-<<<<<<< Updated upstream
-function AppRouter() {
-  return useRoutes([
-    {
-      path: '/',
-      element: <DefaultLayout />,
-      children: [
-        {
-          index: true,
-          path: '/',
-          element: <Home />
-        }
-      ]
-    }
-  ])
-=======
 const routes = createBrowserRouter([
   {
     path: '/',
@@ -48,7 +29,4 @@ const routes = createBrowserRouter([
 
 export default function AppRouter() {
   return <RouterProvider router={routes} />
->>>>>>> Stashed changes
 }
-
-export default AppRouter
